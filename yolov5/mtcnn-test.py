b3f137d3bb7844fa3e25e89693009288d3f9303b
Abhinav Ganguly <derphrrp@gmai.com>
2021-05-06 11:40:35 -0700

    mtcnn test

--- yolov5/mtcnn-test.py
+++ yolov5/mtcnn-test.py
@@ -1,21 +1,11 @@
-import argparse
 import time
-from pathlib import Path
-import os
-
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
-from numpy import random
 
+from PIL import Image, ImageDraw
 import facenet_pytorch
 from facenet_pytorch import MTCNN, InceptionResnetV1
-from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
-    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path, bbox_iou
-from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 from icecream import ic
 
@@ -24,8 +14,6 @@ print('Running on device: {}'.format(elDevice))
 mtcnn = MTCNN(factor=0.25, keep_all=True, device=elDevice)
 
 img = cv2.cvtColor(cv2.imread("/content/spider-sense/ivan_test_pics/white_ivan.png"), cv2.COLOR_BGR2RGB)
-
-mtcnn = MTCNN(factor=0.25, keep_all=True, device=device)
 
 # Get cropped and prewhitened image tensor
 t0 = time.time()
